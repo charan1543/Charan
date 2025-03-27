d2dd5649a10755be248ea28cb8c005d2c27f4ff5
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-03-27 17:24:11 +0530

    primitive_datatype

---
+++ javascript/Datatype/Primitive_datatype.js
@@ -0,0 +1,55 @@
+// Primitive datatypes in JavaScript
+// •	String - A sequence of characters/ Collection of characters. 
+// •	Number - Any number (integer or floating point). 
+// •	BigInt - A type for large integers. 
+// •	Boolean - True or false values. 
+// •	Undefined - Variable has been declared but not assigned a value. 
+// •	Null - Represents the absence of value. 
+// •	Symbol - A unique identifier.Memory allocation (primitive datatypes) (id)
+
+var A = 'This is string'
+
+var B = ' Var is globle Scope'
+
+console.log(A)
+
+console.log(B)
+
+var a = 2 //number data type
+
+// var a ="12" (string with in quotes 12 number)
+
+console.log(a)
+
+var N = true
+var M = false
+
+console.log(N)
+console.log(M)
+
+// operator type (typeof)
+
+console.log(typeof A)
+//  string
+
+console.log(typeof a)
+// number
+
+console.log(typeof N)
+// boolen
+
+console.log(typeof M)
+// boolen
+
+// var j
+
+// console.log(J)
+// Uncaught Reference: J is not defined
+
+var Apple ="Apple"
+const Bannana = Apple
+
+// False & True ( when use with out quotes F & T(False&True) capital latter it's keyword)
+
+const m ="false"
+
