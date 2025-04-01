6266f6db56dac00a66958a106f9bc475992da726
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-01 17:23:40 +0530

    function datatype

---
+++ javascript/Datatype/Functions.js
@@ -0,0 +1,69 @@
+// // Function: It is a block of code to do some task and return some values.
+ 
+ 
+// // Function define:
+// // Function declaration (basic syntax, f.keyword, function name ( ){ code}
+// // Function calling: Function name ( )
+// // function expression 
+// // Named function
+// // annonomous function
+// // arrow function
+
+// function Hello(){
+
+// var a = 10
+// var b = 10
+// console.log (a)
+// console.log (b)
+
+// }
+
+// Hello() //function calling
+// //function declaration
+
+// // named function
+
+// var hello= function Hello(){
+
+//     console.log("named function")
+//     var a = 10
+//     var b = 10
+//     console.log (a)
+//     console.log (b)
+    
+//     }
+    
+//     Hello()
+
+//     //annonomous function 
+//     var a= function (){
+//         var a = 20
+//         var b = 20
+//         console.log (a)
+//         console.log (b)
+//      }
+
+//     a()
+
+//     //Arrow function
+//     var a=() => {
+//         var a = 30
+//         var b = 30
+//         console.log (a)
+//         console.log (b)
+//      }
+
+//      a()
+
+function Hello(){
+
+    var a = 10
+    var b = 10
+    console.log (a)
+    console.log (b)
+
+    return "Hello this is a function"
+    }
+    console.log(Hello())
+    //after return tag function calling not working we need write console.log(function name ( ))
+    
