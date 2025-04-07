7e28d1f8a77bb94fa7f7937f4251e717f4603c73
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-07 17:17:39 +0530

    Control_stetment

---
+++ javascript/Control Stetment/If_else_elseif.js
@@ -0,0 +1,73 @@
+// control statements
+
+// When you have multiple conditions
+
+//If ( only one condition)
+//else
+//if else
+// switch
+
+//syntax: if (condition) {
+
+//Excutes only when the condition is true
+
+// Statements
+// else{
+//Excutes only when the condition is false
+// Statements
+
+var R = 10
+
+var S = 20
+
+if (R>S) {
+
+console.log("R is greater then S")
+
+}
+
+else{
+
+console.log("c value is 1")
+
+}
+
+//if else (
+
+var A=1
+
+if (A === 2 )
+    {
+    console.log("c vlue is 1 ")
+}
+else{
+    console.log("c value is not 1")
+
+}
+
+var B =2 
+
+if (B %2 == 0){
+    console.log("B is event")
+}
+else{
+    console.log("B is not even")
+}
+var C=3
+if (C%2==0){
+    console.log("B is even")
+}
+else {
+    console.log("B is not even ")
+}
+
+var D =10
+if (D ==1){
+    console.log("D value is 1")
+}
+else if (D==2){
+    console.log("D value is 2")
+}
+else {
+    console.log("D value is not 1 or 2")
+}
