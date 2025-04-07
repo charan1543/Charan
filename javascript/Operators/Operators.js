02b8f049a80236e1a10d69a0bd2b3f602bcd87d1
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-07 12:49:00 +0530

    Operators intro

---
+++ javascript/Operators/Operators.js
@@ -0,0 +1,127 @@
+// Operators:-  Operators are used in combination with variables to do a functionality
+
+// Arthemetic operators: +,-,*,%,++,(Pre&Post),--, modular division, **(exponentiation)
+
+// Assignment Operators:
+
+// Comparision Operators:
+
+// Relational:
+
+// Equallity:
+// strict Equallity
+//Loose Equallity
+// In equality:
+// strict Equallity
+//Loose Equallity
+// Ternary Operators:
+
+// Ternary:
+
+// Logical Operators:
+
+// In html(<script src="../"></script>) ../ move to folder with in sub folders
+
+// Then choose (/) subfolder files pr folders it showing
+
+// variable declaration using (var, let, Const).
+
+let a = 10
+
+let b = a
+
+let c = (a+b)
+console.log (c)
+//20
+console.log (c+a)
+
+var fname = "charan"
+var lname = "teja"
+ 
+console.log (fname + lname)
+// concat ( adding two strings)
+
+console.log (typeof(fname))
+//substraction
+console.log (fname - lname)
+// NaN (Not a Number)
+
+console.log(c-b)
+
+//multiplication
+
+console.log ( a*b)
+
+// Quotient (/)
+// percentage (%)
+var A = 100
+var B = 2
+
+console.log (A/B)
+
+console.log (A%B)
+
+// Pre and Post increment
+
+console.log (A+B)
+
+console.log (A+B++)
+
+console.log (A)
+console.log (B)
+
+console.log (++A+B)
+
+console.log (A)
+console.log (B)
+
+// Pre and Post decrement
+
+var D = 20
+var F = 10
+
+console.log (D-- + F++)
+//exponentiation (**)
+
+let G = 2
+let H = 3
+console.log (G**H)
+console.log (G)
+console.log (H)
+
+//assignment Operators = is used to assign a values to variable or constant
+//=, +=,-=, *=, /=, %=, **=
+
+var I = 10
+var J = 20
+console.log (J)
+I += J
+J -= I
+console.log (I/=J)
+
+// Compression Operators
+// <, >, <=, >=.
+
+let K = 10
+let L = 20
+let M = 10
+
+console.log(K<L)
+console.log(K>L)
+console.log (L>K)
+console.log (K=M)
+//10
+console.log (K <=M)
+console.log(K>=M)
+
+// Equallity:
+// strict Equallity (===)
+//Loose Equallity(==)
+// In equality:
+// strict Equallity
+//Loose Equallity
+
+let N=10
+var O='10'
+console.log (N==O)
+console.log (N===O)
