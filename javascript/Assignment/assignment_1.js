10d1b8e72e53a276a927058cae4ae9850111e3b3
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-07 17:52:25 +0530

    7thAssignment

---
+++ javascript/Assignment/assignment_1.js
@@ -0,0 +1,24 @@
+var student_marks =75
+
+if (student_marks  >= 90 && student_marks <= 100){
+    console.log("Excellent")
+
+}
+
+else if (student_marks >= 80 && student_marks <= 89){
+    console.log("Goodjob")
+
+}
+
+else if (student_marks >= 70 && student_marks <= 79){
+    console.log("Needs Improvement")
+
+}
+else if (student_marks >= 60 && student_marks <= 69){
+    console.log("Passing, but Could Do Better")
+
+}
+
+else {
+    console.log ("Failing") 
+}
