78e0933295c0a0ad758e3182c7a47e32a55f199a
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-08 17:39:53 +0530

    08th_april_25_assignment

---
+++ javascript/Assignment/assignment_2.js
@@ -0,0 +1,21 @@
+
+// Print the multiplication table of a number (e.g., 7) from 1 to 10.
+for (let table = 1; table<=10; table++){
+    console.log(table*7)
+}
+
+
+//Sum all the odd numbers between 1 and 50.
+let sum =0
+for (let m = 2; m<=50; m++){
+    if (m % 2 !== 0){
+        sum += m;
+        console.log(sum)
+    }
+  }
+
+//   Print all multiples of 5 from 1 to 100.
+
+for (let a = 1; a<=100; a++){
+    console.log(a*5)
+}
