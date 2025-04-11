eb39b1e2aab32bf09df78128f0cacf4e6a92db0a
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-11 12:20:34 +0530

    Web development Assessment - 3

---
+++ Assignment_11th/Assignment_3.js
@@ -0,0 +1,41 @@
+let a=1
+let b=10
+if ( a>=b){
+    console.log( "a is max number =", a)
+}
+else {
+    console.log ( " b is max Numberm=", b)
+}
+
+let A =10
+let B = 2
+
+if (A+B ==1){
+    console.log ("A+B=",A+B)
+}
+else if (A-B==1){
+    console.log ("A-B=",A-B)
+}
+else if (A*B ==1){
+    console.log ("A*B=",A*B)
+}
+else if (A-B==1){
+    console.log ("A-B=",A-B)
+}
+else {
+    console.log ("A%B=", A%B)
+}
+var num =0
+if (num>0) {
+    console.log ("Positive" )
+}
+else if (num<0){
+    console.log("Negative")
+}
+else if (num ==0){
+    console.log ("Zero")
+}
+else {
+    console.log("Invalid for non-numbers")
+}
+
