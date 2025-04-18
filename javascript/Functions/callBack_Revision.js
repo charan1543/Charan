bee8610474a20f354c6c172407e28142d6bc4527
Charan Teja <charanteja.c@navayuvabharatinfotech.com>
2025-04-18 17:42:30 +0530

    callBack_Revision functions

---
+++ javascript/Functions/callBack_Revision.js
@@ -0,0 +1,29 @@
+function one (){
+    console.log("This is first function")
+}
+
+
+function two (a,b){
+    console.log("This is second function")
+    a()
+}
+two (one)
+
+function dude(c){
+    console.log("this is hello function")
+    console.log("The sum is:",c)
+}
+function c ( a,b,B ){
+    
+    console.log("how are you")
+    return 
+    //  c(a+b)
+     
+}   
+c (1,2,dude)
+
+//pure function
+// easly find the output/ guess the output
+
+//impure function
+// can not find the out put easy way.
